600860696e99d1846aac2ecaa04e7fc1d1a4fa87
nkuthalomakonco <nkuthalomakonco@gmail.com>
2026-01-21 20:35:54 +0200

    public class LoginViewModel : BaseViewModel     {         private string _username;         public string Username;

---
+++ Inventory_POS system/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_POS_system.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string _username;
+        public string Username;
+
+    }
+}
