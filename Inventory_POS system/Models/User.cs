c209eb8a47660f676f48f46e3cce1bee4f8421e8
nkuthalomakonco <nkuthalomakonco@gmail.com>
2026-01-21 20:32:10 +0200

    public class User     {         public int Id { get; set; }         public string Name { get; set; }         public string Surname { get; set; }         public string Email { get; set; }         public string Password { get; set; }         public string UserName { get; set; }         public int Level { get; set; }
        }

---
+++ Inventory_POS system/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_POS_system.Models
+{
+    public class User
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public int Level { get; set; }
+
+    }
+}
